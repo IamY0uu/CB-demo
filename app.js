9a89e19c9f2c00e17bb217e3dba41d13147c52d6
HEMANT SINGH NEGI <168597144+IamY0uu@users.noreply.github.com>
2025-07-04 15:25:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use('/api/v1/books', books);
 
+app.get('/', (req, res) => {
+  res.status(200).send('App is running');
+});
+
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server listening at http://0.0.0.0:${port}`);
 });
